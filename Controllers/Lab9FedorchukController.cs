56fcc7a851dead1453241dc7b686eae381399636
ValentynaFedorchuk <154245348+ValentynaFedorchuk@users.noreply.github.com>
2024-11-13 23:48:47 +0200

    Add files via upload

---
+++ Controllers/Lab9FedorchukController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationlLab9.Controllers
+{
+    public class Lab9FedorchukController : Controller
+    {
+        public IActionResult FirstViewMethod()
+        {
+            List<String> vegetables = GetVegetablesList();
+            return View(vegetables);
+        }
+
+        public List<String> GetVegetablesList() 
+        {
+            List<String> vegetables = new List<String>();
+            vegetables.Add("Помідор");
+            vegetables.Add("Огірок");
+            vegetables.Add("Картопля");
+            vegetables.Add("Баклажан");
+            vegetables.Add("Кабачок");
+            vegetables.Add("Капуста");
+            vegetables.Add("Броколі");
+            vegetables.Add("Квасоля");
+            vegetables.Add("Перець");
+            return vegetables;
+        }
+    }
+}
